6d0d6762bbb68ad2e3e38e763cd72843fd8aaeae
Joao pedro de almeida <almeidajoao@id.uff.br>
2021-04-14 10:53:47 -0300

    adicionando função de ver adjacencia

--- lib/matriz.ex
+++ lib/matriz.ex
@@ -30,6 +30,17 @@ defmodule Matriz do
 
 		printaMatriz(matrizFormatada)
 
+		IO.inspect listaAdjacentes(matrizFormatada, 0,0)
+		IO.inspect listaAdjacentes(matrizFormatada, 0,1)
+		IO.inspect listaAdjacentes(matrizFormatada, 0,2)
+		IO.inspect listaAdjacentes(matrizFormatada, 1,0)
+		IO.inspect listaAdjacentes(matrizFormatada, 1,1)
+		IO.inspect listaAdjacentes(matrizFormatada, 1,2)
+		IO.inspect listaAdjacentes(matrizFormatada, 2,0)
+		IO.inspect listaAdjacentes(matrizFormatada, 2,1)
+		IO.inspect listaAdjacentes(matrizFormatada, 2,2)
+
+
 	end
 
 
@@ -55,7 +66,28 @@ defmodule Matriz do
 		printaMatriz(body)
 	end
 
+	def listaAdjacentes(matriz, i, j) do
+		n = Enum.count(matriz)
+		IO.inspect "vizinhos de #{i},#{j}"
+		retorno = []
+		retorno = adicaoParcial(matriz, i-1, j-1, retorno, n)
+		retorno = adicaoParcial(matriz, i-1, j, retorno, n)
+		retorno = adicaoParcial(matriz, i-1, j+1, retorno, n)
+		retorno = adicaoParcial(matriz, i, j-1, retorno, n)
+		retorno = adicaoParcial(matriz, i, j+1, retorno, n)
+		retorno = adicaoParcial(matriz, i+1, j-1, retorno, n)
+		retorno = adicaoParcial(matriz, i+1, j, retorno, n)
+		retorno = adicaoParcial(matriz, i+1, j+1, retorno, n)
+		retorno
+	end
 
+	def adicaoParcial(matriz, i, j, lista, n) do
+		if(i>=0 && (i+1)<=n && j>=0 && (j+1)<=n) do
+			List.insert_at(lista, -1, get(matriz, i , j))
+		else
+			lista
+		end
+	end
 end
 
 Matriz.inicializa("arquivo.txt")

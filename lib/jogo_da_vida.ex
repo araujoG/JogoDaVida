55e977d39db61166b327cf6f072714d945b5c83e
araujoG <araujo_gabriel@id.uff>
2021-05-02 04:33:52 -0300

    popula matriz funcionando

--- lib/jogo_da_vida.ex
+++ lib/jogo_da_vida.ex
@@ -27,6 +27,138 @@ defmodule JogoDaVida do
 	end
 end
 
+defmodule Matriz do
+	@compile :nowarn_unused_vars
+	@moduledoc """
+	Documentation for `JogoDaVida`.
+	"""
+
+	@doc """
+	Hello world.
+
+	## Examples
+
+		iex> JogoDaVida.hello()
+		:world
+
+	"""
+
+	def inicializa(nome) do
+		input = File.read!(nome)
+		matriz = String.split(input, "\n")
+		n = Enum.count(matriz)
+		matrizFormatada = populaMatriz(matriz, [])
+		printaMatriz(matrizFormatada)
+		IO.inspect "vizinhos de #{0},#{0} => #{listaAdjacentes(matrizFormatada, 0,0)}"
+		# IO.inspect "vizinhos de #{0},#{1} => #{listaAdjacentes(matrizFormatada, 0,1)}"
+		# IO.inspect "vizinhos de #{1},#{1} => #{listaAdjacentes(matrizFormatada, 1,1)}"
+		# IO.inspect listaAdjacentes(matrizFormatada, 0,1)
+		# IO.inspect listaAdjacentes(matrizFormatada, 0,2)
+		# IO.inspect listaAdjacentes(matrizFormatada, 1,0)
+		# IO.inspect listaAdjacentes(matrizFormatada, 1,1)
+		# IO.inspect listaAdjacentes(matrizFormatada, 1,2)
+		# IO.inspect listaAdjacentes(matrizFormatada, 2,0)
+		# IO.inspect listaAdjacentes(matrizFormatada, 2,1)
+		# IO.inspect listaAdjacentes(matrizFormatada, 2,2)
+
+		percorreMatriz(matrizFormatada, 0,0, n)
+
+		# iteracaoPrincipal(matrizFormatada,n, 0, 3)
+
+
+	end
+
+  def populaMatriz([head|tail]) do
+		populaMatriz(tail,List.insert_at([], -1, String.split(head, " ")))
+	end
+
+  def populaMatriz([], matriz) do
+		matriz
+	end
+
+	def populaMatriz([head|tail], matriz) do
+		populaMatriz(tail,List.insert_at(matriz, -1, String.split(head, " ")))
+	end
+
+	def get(matriz, i,j) do
+		{ret, _lixo} = List.pop_at(matriz, i)
+		{ret, _lixo} = List.pop_at(ret, j)
+		ret
+	end
+
+	def printaMatriz([]) do
+	end
+
+	def printaMatriz([head|body]) do
+		IO.inspect head
+		printaMatriz(body)
+	end
+
+  def listaAdjacentes(matriz, i, j) do
+    listaAdjacentes(matriz, i, j, Enum.count(matriz))
+  end
+	def listaAdjacentes(matriz, i, j, n) do
+		[] |> adicaoParcial(matriz, i-1, j-1, n)
+		|> adicaoParcial(matriz, i-1, j, n)
+		|> adicaoParcial(matriz, i-1, j+1, n)
+		|> adicaoParcial(matriz, i, j-1, n)
+		|> adicaoParcial(matriz, i, j+1, n)
+		|> adicaoParcial(matriz, i+1, j-1, n)
+		|> adicaoParcial(matriz, i+1, j, n)
+		|> adicaoParcial(matriz, i+1, j+1, n)
+	end
+
+	def adicaoParcial(lista, matriz, i, j, n) do
+		if(i>=0 && (i+1)<=n && j>=0 && (j+1)<=n) do
+			List.insert_at(lista, -1, get(matriz, i , j))
+		else
+			lista
+		end
+	end
+
+	def percorreMatriz(matriz, i, j, n) do
+		{linhaAtual, _lixo} = List.pop_at(matriz, i)
+		linhaAtual = List.update_at(linhaAtual, j, &(&1 = 0))
+		matriz = List.update_at(matriz,i, &(&1 = linhaAtual)) # Não entendi????
+		if ((j+1)<n) do percorreMatriz(matriz, i, j+1, n)
+		else
+			if ((i+1)<n) do percorreMatriz(matriz, i+1, 0, n)
+			else
+				IO.inspect matriz
+			end
+		end
+	end
+
+	def verificaIgualdadeMatriz(matriz,matrizNova, i, j, n) do #Pode só usar o ==
+		if (get(matriz,i,j) != get(matrizNova,i,j)) do false
+		else
+			if ((j+1)<n) do verificaIgualdadeMatriz(matriz, matrizNova, i, j+1, n)
+			else
+				if ((i+1)<n) do verificaIgualdadeMatriz(matriz, matrizNova, i+1, 0, n)
+				else
+					true
+				end
+			end
+		end
+	end
+
+	def iteracaoPrincipal(matriz, n, iteracoes, iteracoesPrevistas) do
+		if(iteracoes >= iteracoesPrevistas) do
+			IO.inspect ("O sistema Parou depois de #{iteracoes} iteracoes, com a matriz final:")
+			IO.inspect (matriz)
+		else
+			matrizNova = percorreMatriz(matriz,0,0,n)
+			if (matriz == matrizNova) do
+				IO.inspect ("O sistema estabilizou depois de #{iteracoes} iteracoes, com a matriz final:")
+				IO.inspect(matrizNova)
+			else
+				iteracaoPrincipal(matrizNova, n, iteracoes+1, iteracoesPrevistas)
+			end
+		end
+	end
+end
+
+
 defmodule Celula do
 	# Mudança de estado
 	def mudancaDeEstado("Vivo", vizinhos) do
@@ -144,4 +276,5 @@ defmodule Celula do
 		end
 	end
 end
-JogoDaVida.statusTeste()
+# JogoDaVida.statusTeste()
+Matriz.inicializa("arquivo.txt")
--- lib/matriz.ex
+++ lib/matriz.ex
@@ -16,41 +16,39 @@ defmodule Matriz do
 
 	def inicializa(nome) do
 		input = File.read!(nome)
-		matriz = String.split(input, "\r\n")
+		matriz = String.split(input, "\n")
 		n = Enum.count(matriz)
-		IO.inspect n
 		matrizFormatada = populaMatriz(matriz, [])
-		IO.inspect matrizFormatada
-		IO.inspect get(matrizFormatada, 0, 0)
-		IO.inspect get(matrizFormatada, 1, 1)
-		IO.inspect get(matrizFormatada, 2, 2)
-
 		printaMatriz(matrizFormatada)
-
-		IO.inspect listaAdjacentes(matrizFormatada, 0,0)
-		IO.inspect listaAdjacentes(matrizFormatada, 0,1)
-		IO.inspect listaAdjacentes(matrizFormatada, 0,2)
-		IO.inspect listaAdjacentes(matrizFormatada, 1,0)
-		IO.inspect listaAdjacentes(matrizFormatada, 1,1)
-		IO.inspect listaAdjacentes(matrizFormatada, 1,2)
-		IO.inspect listaAdjacentes(matrizFormatada, 2,0)
-		IO.inspect listaAdjacentes(matrizFormatada, 2,1)
-		IO.inspect listaAdjacentes(matrizFormatada, 2,2)
+		IO.inspect "vizinhos de #{0},#{0} => #{listaAdjacentes(matrizFormatada, 0,0)}"
+		# IO.inspect "vizinhos de #{0},#{1} => #{listaAdjacentes(matrizFormatada, 0,1)}"
+		# IO.inspect "vizinhos de #{1},#{1} => #{listaAdjacentes(matrizFormatada, 1,1)}"
+		# IO.inspect listaAdjacentes(matrizFormatada, 0,1)
+		# IO.inspect listaAdjacentes(matrizFormatada, 0,2)
+		# IO.inspect listaAdjacentes(matrizFormatada, 1,0)
+		# IO.inspect listaAdjacentes(matrizFormatada, 1,1)
+		# IO.inspect listaAdjacentes(matrizFormatada, 1,2)
+		# IO.inspect listaAdjacentes(matrizFormatada, 2,0)
+		# IO.inspect listaAdjacentes(matrizFormatada, 2,1)
+		# IO.inspect listaAdjacentes(matrizFormatada, 2,2)
 
 		percorreMatriz(matrizFormatada, 0,0, n)
 
-		iteracaoPrincipal(matrizFormatada,n, 0, 3)
+		# iteracaoPrincipal(matrizFormatada,n, 0, 3)
 
 
 	end
 
+  def populaMatriz([head|tail]) do
+		populaMatriz(tail,List.insert_at([], -1, String.split(head, " ")))
+	end
 
-	def populaMatriz([], matriz) do
+  def populaMatriz([], matriz) do
 		matriz
 	end
 
 	def populaMatriz([head|tail], matriz) do
-		populaMatriz(tail, List.insert_at(matriz, -1, String.split(head, " ")))
+		populaMatriz(tail,List.insert_at(matriz, -1, String.split(head, " ")))
 	end
 
 	def get(matriz, i,j) do
@@ -67,22 +65,21 @@ defmodule Matriz do
 		printaMatriz(body)
 	end
 
-	def listaAdjacentes(matriz, i, j) do
-		n = Enum.count(matriz)
-		IO.inspect "vizinhos de #{i},#{j}"
-		retorno = []
-		retorno = adicaoParcial(matriz, i-1, j-1, retorno, n)
-		retorno = adicaoParcial(matriz, i-1, j, retorno, n)
-		retorno = adicaoParcial(matriz, i-1, j+1, retorno, n)
-		retorno = adicaoParcial(matriz, i, j-1, retorno, n)
-		retorno = adicaoParcial(matriz, i, j+1, retorno, n)
-		retorno = adicaoParcial(matriz, i+1, j-1, retorno, n)
-		retorno = adicaoParcial(matriz, i+1, j, retorno, n)
-		retorno = adicaoParcial(matriz, i+1, j+1, retorno, n)
-		retorno
+  def listaAdjacentes(matriz, i, j) do
+    listaAdjacentes(matriz, i, j, Enum.count(matriz))
+  end
+	def listaAdjacentes(matriz, i, j, n) do
+		[] |> adicaoParcial(matriz, i-1, j-1, n)
+		|> adicaoParcial(matriz, i-1, j, n)
+		|> adicaoParcial(matriz, i-1, j+1, n)
+		|> adicaoParcial(matriz, i, j-1, n)
+		|> adicaoParcial(matriz, i, j+1, n)
+		|> adicaoParcial(matriz, i+1, j-1, n)
+		|> adicaoParcial(matriz, i+1, j, n)
+		|> adicaoParcial(matriz, i+1, j+1, n)
 	end
 
-	def adicaoParcial(matriz, i, j, lista, n) do
+	def adicaoParcial(lista, matriz, i, j, n) do
 		if(i>=0 && (i+1)<=n && j>=0 && (j+1)<=n) do
 			List.insert_at(lista, -1, get(matriz, i , j))
 		else
@@ -93,17 +90,17 @@ defmodule Matriz do
 	def percorreMatriz(matriz, i, j, n) do
 		{linhaAtual, _lixo} = List.pop_at(matriz, i)
 		linhaAtual = List.update_at(linhaAtual, j, &(&1 = 0))
-		matriz = List.update_at(matriz,i, &(&1 = linhaAtual))
+		matriz = List.update_at(matriz,i, &(&1 = linhaAtual)) # Não entendi????
 		if ((j+1)<n) do percorreMatriz(matriz, i, j+1, n)
 		else
 			if ((i+1)<n) do percorreMatriz(matriz, i+1, 0, n)
 			else
-				matriz
+				IO.inspect matriz
 			end
 		end
 	end
 
-	def verificaIgualdadeMatriz(matriz,matrizNova, i, j, n) do
+	def verificaIgualdadeMatriz(matriz,matrizNova, i, j, n) do #Pode só usar o ==
 		if (get(matriz,i,j) != get(matrizNova,i,j)) do false
 		else
 			if ((j+1)<n) do verificaIgualdadeMatriz(matriz, matrizNova, i, j+1, n)
@@ -117,12 +114,12 @@ defmodule Matriz do
 	end
 
 	def iteracaoPrincipal(matriz, n, iteracoes, iteracoesPrevistas) do
-		if(iteracoes >= iteracoesPrevistas) do 
+		if(iteracoes >= iteracoesPrevistas) do
 			IO.inspect ("O sistema Parou depois de #{iteracoes} iteracoes, com a matriz final:")
 			IO.inspect (matriz)
 		else
 			matrizNova = percorreMatriz(matriz,0,0,n)
-			if (verificaIgualdadeMatriz(matriz, matrizNova,0,0,n)) do
+			if (matriz == matrizNova) do
 				IO.inspect ("O sistema estabilizou depois de #{iteracoes} iteracoes, com a matriz final:")
 				IO.inspect(matrizNova)
 			else

e7d983c4343ade9d9862a344038e5beee1cf266b
Joao pedro de almeida <almeidajoao@id.uff.br>
2021-05-04 21:40:28 -0300

    refatorando, adicionando iteracao principal

--- lib/matriz.ex
+++ lib/matriz.ex
@@ -19,18 +19,20 @@ defmodule Matriz do
 
 	def inicializa(nome) do
 		input = File.read!(nome)
+		input = String.replace(input, "\r", "")
 		matriz = String.split(input, "\n")
+
 		n = Enum.count(matriz)
 		matrizFormatada = populaMatriz(matriz, [])
 		printaMatriz(matrizFormatada)
     IO.inspect("-----------------------------")
-    IO.inspect formaLinha([],matrizFormatada, 0, 2)
-    IO.inspect formaLinha([],matrizFormatada, 1, 2)
-    IO.inspect formaLinha([],matrizFormatada, 2, 2)
+    #IO.inspect formaLinha([],matrizFormatada, 0, 2)
+    #IO.inspect formaLinha([],matrizFormatada, 1, 2)
+    #IO.inspect formaLinha([],matrizFormatada, 2, 2)
 		# IO.inspect "vizinhos de #{0},#{0} => #{listaAdjacentes(matrizFormatada, 0,0)}"
 		# IO.inspect "vizinhos de #{0},#{1} => #{listaAdjacentes(matrizFormatada, 0,1)}"
 		# IO.inspect "vizinhos de #{1},#{1} => #{listaAdjacentes(matrizFormatada, 1,1)}"
-		# IO.inspect listaAdjacentes(matrizFormatada, 0,1)
+		#IO.inspect listaAdjacentes(matrizFormatada, 0,1)
 		# IO.inspect listaAdjacentes(matrizFormatada, 0,2)
 		# IO.inspect listaAdjacentes(matrizFormatada, 1,0)
 		# IO.inspect listaAdjacentes(matrizFormatada, 1,1)
@@ -39,9 +41,9 @@ defmodule Matriz do
 		# IO.inspect listaAdjacentes(matrizFormatada, 2,1)
 		# IO.inspect listaAdjacentes(matrizFormatada, 2,2)
 
-		# percorreMatriz(matrizFormatada, 0,0, n)
+		#IO.inspect percorreMatriz([],matrizFormatada, 0,0, n)
 
-		# iteracaoPrincipal(matrizFormatada,n, 0, 3)
+		iteracaoPrincipal(matrizFormatada,n, 1, 6)
 
 
 	end
@@ -95,17 +97,26 @@ defmodule Matriz do
 	end
 
   def formaLinha(novaLinha, matriz, i, j) when j == 0 do
-    {linhaAtual, _lixo} = List.pop_at(matriz, i)
-    {elemento, _lixo} = List.pop_at(linhaAtual, j)
-    [mudancaDeEstado(elemento, listaAdjacentes(matriz,i,j)) | novaLinha]
+    elemento = get(matriz,i,j)
+    [Celula.mudancaDeEstado(elemento, listaAdjacentes(matriz,i,j)) | novaLinha]
   end
 
   def formaLinha(novaLinha, matriz, i, j) do
-    {linhaAtual, _lixo} = List.pop_at(matriz, i)
-    {elemento, _lixo} = List.pop_at(linhaAtual, j)
-    [mudancaDeEstado(elemento, listaAdjacentes(matriz,i,j)) | novaLinha] |>
+		elemento = get(matriz,i,j)
+    [Celula.mudancaDeEstado(elemento, listaAdjacentes(matriz,i,j)) | novaLinha] |>
     formaLinha(matriz, i, j-1)
   end
+
+
+
+	def percorreMatriz(matrizNova,matriz, i, j, n) do
+		linhaAtualizada = formaLinha([], matriz, i, n-1)
+		matrizNova = List.insert_at(matrizNova, -1, linhaAtualizada)
+		if ((i+1)<n) do percorreMatriz(matrizNova, matriz, i+1, 0, n)
+			else
+				matrizNova
+			end
+	end
 
 	def verificaIgualdadeMatriz(matriz,matrizNova, i, j, n) do #Pode só usar o ==
 		if (get(matriz,i,j) != get(matrizNova,i,j)) do false
@@ -120,19 +131,21 @@ defmodule Matriz do
 		end
 	end
 
-	# def iteracaoPrincipal(matriz, n, iteracoes, iteracoesPrevistas) do
-	# 	if(iteracoes >= iteracoesPrevistas) do
-	# 		IO.inspect ("O sistema Parou depois de #{iteracoes} iteracoes, com a matriz final:")
-	# 		IO.inspect (matriz)
-	# 	else
-	# 		matrizNova = percorreMatriz(matriz,0,0,n)
-	# 		if (matriz == matrizNova) do
-	# 			IO.inspect ("O sistema estabilizou depois de #{iteracoes} iteracoes, com a matriz final:")
-	# 			IO.inspect(matrizNova)
-	# 		else
-	# 			iteracaoPrincipal(matrizNova, n, iteracoes+1, iteracoesPrevistas)
-	# 		end
-	# 	end
-	# end
+	def iteracaoPrincipal(matriz, n, iteracoes, iteracoesPrevistas) do
+	 	if(iteracoes >= iteracoesPrevistas) do
+	 		IO.inspect ("O sistema Parou depois de #{iteracoes} iteracoes, com a matriz final:")
+	 		IO.inspect (matriz)
+	 	else
+	 		matrizNova = percorreMatriz([], matriz,0,0,n)
+	 		if verificaIgualdadeMatriz(matriz, matrizNova, 0,0,n) do
+	 			IO.inspect ("O sistema estabilizou depois de #{iteracoes} iteracoes, com a matriz final:")
+	 			IO.inspect(matrizNova)
+	 		else
+				IO.inspect("Sistema ainda em execução, iteração #{iteracoes}, matriz atual:")
+				IO.inspect(matrizNova)
+	 			iteracaoPrincipal(matrizNova, n, iteracoes+1, iteracoesPrevistas)
+	 		end
+	 	end
+	 end
 end
 Matriz.inicializa("arquivo.txt")
